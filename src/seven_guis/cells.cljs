5ec03f3fa707dc0d735350e88666fbdee825a483
Estevo U. Caamaño Castro <euccastro@gmail.com>
2020-12-02 00:30:18 +0100

    factor out cell-editor, activate on any non-nil value

--- src/seven_guis/cells.cljs
+++ src/seven_guis/cells.cljs
@@ -10,27 +10,32 @@
 (def cols "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 
+(defn cell-editor [edit-text cursor]
+  [:input
+   {:type :text
+    :auto-focus true
+    :on-focus #(.. % -currentTarget select)
+    :on-key-down #(case (.-keyCode %)
+                    ;; commit and exit on ENTER
+                    (13) (.. % -target blur)
+                    ;; cancel on ESC
+                    (27) (reset! edit-text nil)
+                    true)
+    :value @edit-text
+    :on-blur #(let [t @edit-text]
+                (when (string? t)
+                  (reset! cursor t)
+                  (reset! edit-text nil)))
+    :on-change #(reset! edit-text (util/evt-value %))}])
+
+
 (defn cell [k cursor]
   (let [edit-text (r/atom nil)]
     (fn [k cursor]
-      [:td {:title k
+      [:td {:title k  ; tooltip to double-check you're editing the right cell
             :on-double-click #(reset! edit-text (or @cursor ""))}
-       (if (string? @edit-text)
-         [:input {:type :text
-                  :auto-focus true
-                  :on-focus #(.. % -currentTarget select)
-                  :on-key-down #(case (.-keyCode %)
-                                  ;; commit and exit on ENTER
-                                  (13) (.. % -target blur)
-                                  ;; cancel on ESC
-                                  (27) (reset! edit-text nil)
-                                  true)
-                  :value @edit-text
-                  :on-blur #(let [t @edit-text]
-                              (when (string? t)
-                               (reset! cursor t)
-                               (reset! edit-text nil)))
-                  :on-change #(reset! edit-text (util/evt-value %))}]
+       (if (some? @edit-text)
+         [cell-editor edit-text cursor]
          @cursor)])))
 
 
