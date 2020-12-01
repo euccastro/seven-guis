7daa2af54c624f40a606e8169bc1ee918f35a8d6
Estevo U. Caamaño Castro <euccastro@gmail.com>
2020-12-02 00:22:49 +0100

    make cells more autonomous
    Pass cursors in, let them handle transient edit state.  Until we implement
    formulas, only the editing cell should be rerendered now.

--- src/seven_guis/cells.cljs
+++ src/seven_guis/cells.cljs
@@ -10,20 +10,36 @@
 (def cols "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 
-(defn cell [k contents state]
-  [:td {:title k
-        :on-double-click #(swap! state assoc :editing-cell k)}
-   (if (= k (:editing-cell @state))
-     [:input {:type :text
-              :value contents
-              :on-blur #(swap! state dissoc :editing-cell)
-              :on-change #(swap! state assoc-in [:cell-contents k] (util/evt-value %))}]
-     contents)])
+(defn cell [k cursor]
+  (let [edit-text (r/atom nil)]
+    (fn [k cursor]
+      [:td {:title k
+            :on-double-click #(reset! edit-text (or @cursor ""))}
+       (if (string? @edit-text)
+         [:input {:type :text
+                  :auto-focus true
+                  :on-focus #(.. % -currentTarget select)
+                  :on-key-down #(case (.-keyCode %)
+                                  ;; commit and exit on ENTER
+                                  (13) (.. % -target blur)
+                                  ;; cancel on ESC
+                                  (27) (reset! edit-text nil)
+                                  true)
+                  :value @edit-text
+                  :on-blur #(let [t @edit-text]
+                              (when (string? t)
+                               (reset! cursor t)
+                               (reset! edit-text nil)))
+                  :on-change #(reset! edit-text (util/evt-value %))}]
+         @cursor)])))
 
 
 (defn counter []
-  (let [state (r/atom {:cell-contents {}
-                       :editing-cell nil})]
+  (let [state (r/atom {:cell-contents {}})
+        cursors (into {}
+                      (for [row rows
+                            col cols]
+                        [[row col] (r/cursor state [:cell-contents [row col]])]))]
     (fn []
       [:table
        [:tr
@@ -36,10 +52,9 @@
          [:tr
           [:th row]
           (for [col cols
-                :let [k (str col row)
-                      cell-contents (get-in @state [:cell-contents k])]]
-            ^{:key k}
-            [cell k cell-contents state])])])))
+                :let [k (str row col)]]
+            ^{:key (str col row)}
+            [cell k (cursors [row col])])])])))
 
 
 (rdom/render [counter]
